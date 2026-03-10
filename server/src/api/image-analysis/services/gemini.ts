8a0270f39aa0d0564df52830584a18eaf49f362d
Ayush Surya <ayushsurya204@gmail.com>
2026-03-10 11:14:12 +0530

    second commit

--- server/src/api/image-analysis/services/gemini.ts
+++ server/src/api/image-analysis/services/gemini.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 import fs from "fs";
 
-const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI });
 
 export const analyzeImage = async (_filePath: string) => {
   try {
